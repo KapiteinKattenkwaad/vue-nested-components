7c0afb206fcac8c76274ec9aa531ab92ca675834
Max St <maxstouten@hotmail.com>
2019-02-28 07:19:35 +0100

    ninjas nested components

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <ninjas></ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+
+import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'ninjas': Ninjas
+  },
+  data () {
+    return {
+      title: 'This is the sound of the police'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+      <p> {{ copyright }} </p>
+  </footer>
+</template>
+
+<script>
+export default {
+ 
+  data () {
+      return {
+        copyright: 'Made by Max 2019'
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+footer {
+    background:  #222;
+    padding: 6px;
+    bottom: 10px;
+}
+p {
+    color: rgb(204, 214, 204);
+    text-align: center;
+}
+
+</style>
